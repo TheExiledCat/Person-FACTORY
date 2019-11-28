622109e559941609d16497f6aa496a75023b3d4a
TheExiledCat <43574582+TheExiledCat@users.noreply.github.com>
2019-11-28 09:29:15 +0100

    added

---
+++ Users/User.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <string>
+class User
+{
+public:
+	User();
+	~User();
+	std::string GetName() {
+
+		return username;
+
+	}
+
+	void SetName(std::string name) {
+		username = name;
+	}
+
+private:
+	std::string username;
+	std::string password;
+	int userID;
+
+
+
+};
+
